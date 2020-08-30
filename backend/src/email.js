d1f3fc85f1e15c4d91b21d3d23b7e024379bddfd
priyankavidhate <64757581+priyankavidhate@users.noreply.github.com>
2020-08-30 12:12:04 -0700

    refactor sendOrder email notification

--- backend/src/email.js
+++ backend/src/email.js
@@ -74,58 +74,59 @@ async function send(mail) {
   });
 }
 
-// async function sendOrder(from, to, org, type, status, id) {
+async function sendOrder(from, to, org, type, status, id) {
 
-//   const from_email = new helper.Email(from);
-//   const to_email = new helper.Email(to);
-//   const subject = 'welcome to the Project';
+  const from_email = new helper.Email(from);
+  const to_email = new helper.Email(to);
+  const subject = 'welcome to the Project';
 
-//  if(type == 'new order') {
-//     body = 'Organization ' + org + ' has placed new order for you';
-//   }
+ if(type == 'new order') {
+    body = 'Organization ' + org + ' has placed new order for you';
+  }
 
-//   else if(type == 'status') {
-//     body = 'Status for Order ' + id ' has changed. Please open app for more details';
-//   }
+  else if(type == 'status') {
+    body = 'Status for Order ' + id ' has changed. Please open app for more details';
+  }
 
-//   let content = new helper.Content('text/plain', body);
-//   let mail = new helper.Mail(from_email, subject, to_email, content);
+  let content = new helper.Content('text/plain', body);
+  let mail = new helper.Mail(from_email, subject, to_email, content);
 
-//   let sg = sendgrid(process.env.SENDGRID_API_KEY);
-//   let request = sg.emptyRequest({
-//     method: 'POST',
-//     path: '/v3/mail/send',
-//     body: mail.toJSON(),
-//   });
+  let sg = sendgrid(process.env.SENDGRID_API_KEY);
+  let request = sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: mail.toJSON(),
+  });
 
-//   sg.API(request, function(error, response) {
-//     debug('statusCode : %o', response.statusCode);
-//     debug('body : %o', response.body);
-//     debug('headers : %o', response.headers);
-//   });
+  sg.API(request, function(error, response) {
+    debug('statusCode : %o', response.statusCode);
+    debug('body : %o', response.body);
+    debug('headers : %o', response.headers);
+  });
 
-//   if(type == 'new order') {
+  if(type == 'new order') {
 
-//     body = 'You placed an order ' + org + ' has placed new order for you';
+    body = 'You placed an order ' + org + ' has placed new order for you';
 
-//     content = new helper.Content('text/plain', body);
-//     mail = new helper.Mail(from_email, subject, to_email, content);
+    content = new helper.Content('text/plain', body);
+    mail = new helper.Mail(from_email, subject, to_email, content);
 
-//     sg = sendgrid(process.env.SENDGRID_API_KEY);
-//     request = sg.emptyRequest({
-//       method: 'POST',
-//       path: '/v3/mail/send',
-//       body: mail.toJSON(),
-//     });
+    sg = sendgrid(process.env.SENDGRID_API_KEY);
+    request = sg.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: mail.toJSON(),
+    });
 
-//     sg.API(request, function(error, response) {
-//       debug('statusCode : %o', response.statusCode);
-//       debug('body : %o', response.body);
-//       debug('headers : %o', response.headers);
-//     });
+    sg.API(request, function(error, response) {
+      debug('statusCode : %o', response.statusCode);
+      debug('body : %o', response.body);
+      debug('headers : %o', response.headers);
+    });
 
-//    }
+   }
 
+module.exports.sendOrder = sendOrder;   
 module.exports.sendAccount = sendAccount;
 module.exports.confirmAccount = confirmAccount;
 
